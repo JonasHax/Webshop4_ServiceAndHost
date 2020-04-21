4b60a5b8e62cb9f2495671258e8ef54aeaf6ef56
Jonas Haxholm <jonashaxholm@hotmail.dk>
2020-04-21 14:28:53 +0200

    Ændring i order

--- Services/Model/Order.cs
+++ Services/Model/Order.cs
@@ -16,8 +16,11 @@ namespace Services.Model {
         public int CustomerId { get; set; }
         [DataMember]
         public DateTime Date { get; set; }
+        [DataMember]
+        public List<SalesLineItem> SalesLineItems { get; set; }
 
         public Order() {
+            SalesLineItems = new List<SalesLineItem>();
         }
-    }
+    }   
 }
